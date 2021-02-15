ab68dc186877bd87057786c6e9727b6220a026c9
Mehraj Bhat <mehrajb33@gmail.com>
2021-02-15 21:48:34 +0530

    FIxed Some issue

--- app/src/main/java/com/science/androidservice/MainActivity.java
+++ app/src/main/java/com/science/androidservice/MainActivity.java
@@ -1,8 +1,10 @@
 package com.science.androidservice;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void onClick(View src) {
         switch (src.getId()) {
             case R.id.buttonStart:
--- app/src/main/java/com/science/androidservice/MyService.java
+++ app/src/main/java/com/science/androidservice/MyService.java
@@ -1,34 +1,88 @@
 package com.science.androidservice;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+    public int counter=0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, new Notification());
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void startMyOwnForeground()
+    {
+        String NOTIFICATION_CHANNEL_ID = "example.permanence";
+        String channelName = "Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
+
     MediaPlayer myPlayer;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        myPlayer = MediaPlayer.create(this, R.raw.song);
+        myPlayer.setLooping(false);
+        myPlayer.start();
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        myPlayer.stop();
+//        Intent broadcastIntent = new Intent();
+//        broadcastIntent.setAction("restartservice");
+//        broadcastIntent.setClass(this, Restarter.class);
+//        this.sendBroadcast(broadcastIntent);
+
+    }
+
+
+
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-    @Override
-    public void onCreate() {
-        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
-        myPlayer = MediaPlayer.create(this, R.raw.song);
-        myPlayer.setLooping(false); // Set looping
-    }
-    @Override
-    public void onStart(Intent intent, int startid) {
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
-        myPlayer.start();
-    }
-    @Override
-    public void onDestroy() {
-        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
-        myPlayer.stop();
     }
 }
