5b738f8b3868f81648fa45aabc1084003edadc3a
Mehraj Bhat <mehrajb33@gmail.com>
2021-02-15 14:28:44 +0530

    Anroid Service Demonstartion

---
+++ app/src/main/java/com/science/androidservice/NextPage.java
@@ -0,0 +1,15 @@
+package com.science.androidservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_page);
+
+    }
+}
